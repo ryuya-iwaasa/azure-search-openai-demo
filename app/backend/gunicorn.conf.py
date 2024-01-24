9a3ac4ad407e549ad6c53834f752604ff29fe7c0
ryuya-iwaasa <r-iwaasa@core.co.jp>
2024-01-24 15:30:16 +0900

    commit_08

--- app/backend/gunicorn.conf.py
+++ app/backend/gunicorn.conf.py
@@ -3,9 +3,9 @@ import multiprocessing
 max_requests = 1000
 max_requests_jitter = 50
 log_file = "-"
-bind = "0.0.0.0:8000"
+bind = "0.0.0.0"
 
-timeout = 1200
+timeout = 230
 # https://learn.microsoft.com/en-us/troubleshoot/azure/app-service/web-apps-performance-faqs#why-does-my-request-time-out-after-230-seconds
 
 num_cpus = multiprocessing.cpu_count()
